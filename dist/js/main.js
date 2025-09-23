03953d50455d9a1764566c0983744347f3339b5a
Gareynov Grigoriy <g.gareinov@fact.digital>
2025-09-24 00:06:57 +0500

    add input number and history

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,46 @@
-console.log("start");
+"use strict";
+const startRange = 1;
+const endRange = 100;
+let historyNumber = [];
+let randomNumber = (Math.random() * 100).toFixed();
+console.log(randomNumber);
+
+const startRangeElement = document.querySelector(".start__range");
+startRangeElement.innerHTML = startRange;
+
+const endRangeElement = document.querySelector(".end__range");
+endRangeElement.innerHTML = endRange;
+
+const getNumberInput = document.querySelector(".enter__number");
+
+// getNumberInput.addEventListener("input", () => {
+//   console.log(getNumberInput.value);
+// });
+
+const getCheckNumberBtn = document.querySelector(".check__btn");
+const getHistoryBlock = document.querySelector(".history__list");
+
+getCheckNumberBtn.addEventListener("click", () => {
+  if (
+    getNumberInput.value &&
+    +getNumberInput.value >= startRange &&
+    +getNumberInput.value <= endRange
+  ) {
+    historyNumber.push(getNumberInput.value);
+    getHistoryBlock.innerHTML = historyNumber.toString();
+    // getHistoryBlock.innerHTML += `${getNumberInput.value}, `;
+    console.log(parseInt(getNumberInput.value));
+    getNumberInput.value = "";
+  }
+});
+
+const getNewNumberBtn = document.querySelector(".new__number__btn");
+
+getNewNumberBtn.addEventListener("click", () => {
+  historyNumber = [];
+  getNumberInput.value = "";
+  getHistoryBlock.innerHTML = "";
+  randomNumber = (Math.random() * 100).toFixed();
+  console.log("История очищена и создано новое число");
+  console.log(randomNumber);
+});
